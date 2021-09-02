b0233f7bfb513dbff3e93a182e69043091641b09
iftakherone <54303402+iftakherone@users.noreply.github.com>
2021-09-02 14:09:49 +0600

    displaying search result

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const toggleLoader = displayStyle => {
     document.getElementById('loader').style.display = displayStyle;
-}
+};
 
 const toggleSearchResult = displayStyle => {
     document.getElementById('display-result').style.display = displayStyle;
-}
+};
 
 const getSearchInput = () => {
     const searchField = document.getElementById('search-field');
@@ -26,22 +26,42 @@ const getSearchInput = () => {
                 }
                 return res.json();
             })
-            .then((data) => console.log(data));
+            .then((data) => displaySearchResult(data.docs));
     }
-}
+};
 
-document.getElementById('search-btn').addEventListener('click', function() {
+
+document.getElementById('search-btn').addEventListener('click', function () {
     getSearchInput();
-})
+});
 
-const displaySearchResult = () => {
+const displaySearchResult = docs => {
+    const displayResult = document.getElementById('display-result');
+    displayResult.textContent = '';
+    const div = document.createElement('div');
+    let i = 0;
+
+    if (docs == null || docs.length == 0) {
+        alert('No result found!');
+        return;
+    }
     
-}
-
-
-
-/* 
-after appendChild of div
-toggleLoader('none');
-toggleSearchResult('block');
-*/
+    docs.forEach(doc => {
+        i++
+        console.log(doc);
+        div.classList.add('col-6');
+        div.innerHTML = `
+            <div class="card box-shadow">
+                <div class="card-body">
+                    <h4 class="card-title">${doc.title}</h4>
+                    <h6 class="card-text"><strong>Author:</strong> ${doc.author_name}</h6>
+                    <h6 class="card-text"><strong>By:</strong> ${doc.publisher}</h6>
+                    <a href="#info-display" class="btn bg-lavender">Learn More</a>
+                </div>
+            </div>
+        `;
+        displayResult.appendChild(div);
+        toggleLoader('none');
+        toggleSearchResult('block');
+    });
+};
