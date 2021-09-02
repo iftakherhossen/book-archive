970e8b948a049e352653f7674de0ba8e2e226131
iftakherone <54303402+iftakherone@users.noreply.github.com>
2021-09-02 19:21:47 +0600

    displaying result

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
+// Loader Function 
 const toggleLoader = displayStyle => {
     document.getElementById('loader').style.display = displayStyle;
 };
 
+// Toggle Search Result
 const toggleSearchResult = displayStyle => {
     document.getElementById('display-result').style.display = displayStyle;
 };
 
+// Counter Function
+const toggleCounter = displayStyle => {
+    document.getElementById('counter').style.display = displayStyle;
+}
+toggleCounter('none');
+
+// Search Input Function
 const getSearchInput = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     searchField.value = '';
     toggleLoader('flex');
-    toggleSearchResult('none')
+    toggleSearchResult('none');
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
 
     if (searchText == '') {
@@ -31,14 +40,16 @@ const getSearchInput = () => {
 };
 
 
+// Button Event Listener
 document.getElementById('search-btn').addEventListener('click', function () {
     getSearchInput();
 });
 
+// Search Result Function
 const displaySearchResult = docs => {
     const displayResult = document.getElementById('display-result');
     displayResult.textContent = '';
-    const div = document.createElement('div');
+    const counterNum = document.getElementById('counter-number');
     let i = 0;
 
     if (docs == null || docs.length == 0) {
@@ -46,22 +57,28 @@ const displaySearchResult = docs => {
         return;
     }
     
+    toggleCounter('block');
     docs.forEach(doc => {
-        i++
-        console.log(doc);
-        div.classList.add('col-6');
+        i++;
+        const div = document.createElement('div');
+        div.classList.add('col');
         div.innerHTML = `
             <div class="card box-shadow">
+                <img src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top height" alt="Book Cover Image">
                 <div class="card-body">
                     <h4 class="card-title">${doc.title}</h4>
                     <h6 class="card-text"><strong>Author:</strong> ${doc.author_name}</h6>
-                    <h6 class="card-text"><strong>By:</strong> ${doc.publisher}</h6>
-                    <a href="#info-display" class="btn bg-lavender">Learn More</a>
+                    <h6 class="card-text"><strong>Published By:</strong> ${doc.publisher}</h6>
+                    <h6 class="card-text"><strong>First Publish:</strong> ${doc.publish_year}</h6>
+                    <h6 class="card-text"><strong>Language:</strong> ${doc.language}</h6>
+                    <button onclick="displayInfo()" class="btn bg-lavender mt-3">LEARN MORE</button>
                 </div>
             </div>
         `;
         displayResult.appendChild(div);
         toggleLoader('none');
-        toggleSearchResult('block');
+        toggleSearchResult('flex');
     });
+
+    counterNum.innerText = i;
 };
